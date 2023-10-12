782058faae406fdcbeccccc386ba503278604338
Cyril Derc <cyril_derc@hotmail.com>
2023-10-12 13:55:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const string = "Hello les gens";
+
+// console.log(string.split(" ").reverse().join(""));
+
+// console.log(string.split());
+
+function reverString(str) {
+  console.log(str.split("").reverse().join(""));
+}
+
+const string = "hello les gens";
+
+reverString(string);
