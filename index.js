0fbda9ae5f80bd240225c2064136ebfae819ca1c
Cyril Derc <cyril_derc@hotmail.com>
2023-10-12 15:12:31 +0200

    index.js - palindrome

--- index.js
+++ index.js
@@ -7,3 +7,17 @@
 // const string = "hello les gens";
 
 // reverString(string);
+
+// ========================================================================= vérifier si un mot est un palindrome (il peut s'écrire dans les deux sens) ======================================================
+
+// function palindrome(wrd) {
+//   if (wrd === wrd.split("").reverse().join("")) {
+//     console.log("c'est un palindrome");
+//   } else {
+//     console.log("ce n'est pas un palindrome");
+//   }
+// }
+
+// const word = "laval";
+
+// palindrome(word);
