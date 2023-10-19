288c8ad3195f612189222b825123e155313627c5
Cyril Derc <cyril_derc@hotmail.com>
2023-10-19 15:40:02 +0200

    exo

--- index.js
+++ index.js
@@ -237,3 +237,8 @@
 // highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
 
 // In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// explications :
+// 1/ on doit transformer la chaine de caractères en tableau pour pouvoir trouver le chiffre. Pour cela on utilise .split()
+// 2/ comme le paramètre est une chaine de caractères on utilise ... pour le transformer en Array.
+// 3/ on devra utiliser numbers.split(" ") deux fois. Pour éviter la répétition on peut le passer en argument.
