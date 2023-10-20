bbf774cdd4bae61122f4f1ed780b697d42b9a2d7
Cyril Derc <cyril_derc@hotmail.com>
2023-10-20 06:13:54 +0200

    exo

--- index.js
+++ index.js
@@ -310,3 +310,5 @@
 // filter_list([1, 2, "a", "b"]);
 
 // In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// ========================================
